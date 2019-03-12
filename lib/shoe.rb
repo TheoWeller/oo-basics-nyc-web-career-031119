f977cd04d48e07afec84fc803ee8185993b18fb7
Theo Weller <weller.theodor@gmail.com>
2019-03-12 21:37:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,7 +5,7 @@ class Shoe
   end
   attr_accessor :brand, :color, :name, :size, :material, :condition
 
-  def cobble(arg)
+  def cobble
     self.condition="new"
     puts "Your shoe is as good as new!"
 
